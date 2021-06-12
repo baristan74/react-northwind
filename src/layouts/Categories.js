5a6deb1b77816576b349fb85331d5398b6502927
Barış Tan <61651202+baristan74@users.noreply.github.com>
2021-06-13 02:42:36 +0300

    Day 2

--- src/layouts/Categories.js
+++ src/layouts/Categories.js
@@ -1,20 +1,14 @@
-import React from 'react'
-import { Menu } from 'semantic-ui-react'
+import React from "react";
+import { Menu } from "semantic-ui-react";
 
 export default function Categories() {
-    return (
-        <div>
-            <Menu pointing vertical>
-        <Menu.Item
-          name='home'
-        />
-        <Menu.Item
-          name='messages'
-        />
-        <Menu.Item
-          name='friends'
-        />
+  return (
+    <div>
+      <Menu pointing vertical>
+        <Menu.Item name="home" />
+        <Menu.Item name="messages" />
+        <Menu.Item name="friends" />
       </Menu>
-        </div>
-    )
+    </div>
+  );
 }
--- src/layouts/Navi.js
+++ src/layouts/Navi.js
@@ -1,22 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import CartSummary from "./CartSummary";
 import { Button, Container, Menu } from "semantic-ui-react";
+import SignedIn from "./SignedIn";
+import SignedOut from "./SignedOut";
+import { useHistory } from "react-router";
 
 export default function Navi() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const history =useHistory()
+
+  function handleSignOut() {
+    setIsAuthenticated(false)
+    history.push("/")
+
+  }
+
+  function handleSignIn(){
+    setIsAuthenticated(true)
+  }
+
   return (
     <div>
-      <Menu inverted >
+      <Menu inverted>
         <Container>
           <Menu.Item name="home" />
           <Menu.Item name="messages" />
 
           <Menu.Menu position="right">
-
             <CartSummary />
-
-            <Menu.Item>
-              <Button primary>Sign Up</Button>
-            </Menu.Item>
+            {isAuthenticated ? <SignedIn signOut={handleSignOut} bisey="1" /> : <SignedOut signIn={handleSignIn}/>}
           </Menu.Menu>
         </Container>
       </Menu>
